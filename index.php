99a4e733c48399f190e1955f823835c2b15d4bca
hpmadan <46838105+hpmadan@users.noreply.github.com>
2019-01-19 14:08:10 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
 <h1>This is my PHP Test Application<h1>
   <p> This is my aws bean stalk paas</p>
   <p1> hey boss code is ready</p1>
-  <p> shankar direction new movie is idian 2  </p>
+  <p> shankar direction new movie is indian 2  </p>
+  <p> KGF Movie is also good average</p>
   <p> Anthariksham 9000kmph viprayana moon satellite ,water found in moon space openly successeded welcome to the world of the php code of the life of the daty </p>
 
 <?php
